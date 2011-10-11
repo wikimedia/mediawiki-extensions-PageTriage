943cfde4ef2f059c6bf389032d4db73fa7930cb9
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-10-11 20:13:37 +0000

    fixing sloppy js errors, follow-up to r99454 and r99549

--- ext.pageTriage.core.js
+++ ext.pageTriage.core.js
@@ -1,29 +1,38 @@
 ( function( $ ) { 
 
-var	$currentArticle = null, // the title of the current article being reviewed
+var	$currentArticle = null; // the title of the current article being reviewed
 	
 $.pageTriage = {
 	tagArticle: function() {
+		var tagsToApply = new Array;
+		$.each( $( '#ptr-checkboxes input:checkbox' ), function( index, value ) {
+			if ( $( this ).is( ':checked' ) ) {
+				// Add it to the list
+			}
+		} );
+		
 		var sendData = {
 			'action': 'edit',
 			'title': $currentArticle,
 			'text' : $newText,
 			'token': $token,
-			'summary': 'Triaging the page,
-			'notminor': true,
+			'summary': 'Triaging the page',
+			'notminor': true
 		};
 
 		$.ajax( {
-			url: mw.config.get( 'wgScriptPath' ) + '/api.php',
+			url: mw.util.wikiScript( 'api' ),
 			data: sendData,
 			dataType: 'json',
-			type: 'POST',
+			type: 'POST'
 		} );
 	},
 
-	loadArticle: function() {
+	loadPage: function() {
+		$( '#ptr-stuff' ).append( "Article goes here!" );
 		// Load in an article
 	}
+};
 
-	$( document ).ready( $.pageTriage.loadArticle );
-}
+$( document ).ready( $.pageTriage.loadPage );
+} ) ( jQuery );
