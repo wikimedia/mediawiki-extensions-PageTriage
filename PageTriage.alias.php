2ec81181e2d0c6a0860372acd541028abf50e4b7
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-10-11 00:08:33 +0000

    adding alias file and i18n file

---
+++ PageTriage.alias.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Aliases for Special:PageTriage
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author  Ryan Kaldari
+ */
+$specialPageAliases['en'] = array(
+	'PageTriage' => array( 'PageTriage' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
---
+++ PageTriage.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for Page Triage extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Ryan Kaldari
+ */
+$messages['en'] = array(
+	'pagetriage-desc' => 'Facilitates reviwing and approving new articles',
+	'pagetriage' => 'Page Triage',
+);
