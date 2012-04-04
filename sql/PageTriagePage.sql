e9ee7dc9f07796cceec6c8392fa6cc7ac2c7a0b4
Ryan Kaldari <kaldari@users.mediawiki.org>
2012-04-04 01:53:26 +0000

    adding comment to clarify ambiguous field name

--- sql/PageTriagePage.sql
+++ sql/PageTriagePage.sql
@@ -2,7 +2,7 @@
 CREATE TABLE /*_*/pagetriage_page (
 	ptrp_page_id int unsigned NOT NULL PRIMARY KEY,
 	ptrp_triaged tinyint unsigned not null default 0,
-	ptrp_timestamp varbinary(14) NOT NULL
+	ptrp_timestamp varbinary(14) NOT NULL COMMENT 'when the page was created'
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/ptrp_triaged_timestamp_page_id ON /*_*/pagetriage_page (ptrp_triaged, ptrp_timestamp, ptrp_page_id);
