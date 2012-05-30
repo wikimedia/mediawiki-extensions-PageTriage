a7ef696cbd808ddbd1880773c6ade7c70aa00ff8
Kaldari <rkaldari@wikimedia.org>
2012-05-30 11:09:55 -0700

    Restoring non-curation-bar reviewing interface
    Change-Id: I7482167b6026672183f69d4d60a8efd358009a59

--- PageTriage.hooks.php
+++ PageTriage.hooks.php
@@ -239,13 +239,14 @@ class PageTriageHooks {
 			return true;
 		}
 
-		// If the curation toolbar is disabled, no need to go further
-		if ( !$wgPageTriageEnableCurationToolbar ) {
+		// don't show anything for user with no patrol right
+		if ( !$article->getTitle()->quickUserCan('patrol') ) {
 			return true;
 		}
 
-		// don't show anything for user with no patrol right
-		if ( !$article->getTitle()->quickUserCan('patrol') ) {
+		// Only show in article and user namespaces
+		$namespace = $article->getTitle()->getNamespace();
+		if ( $namespace !== NS_MAIN && $namespace !== NS_USER ) {
 			return true;
 		}
 
@@ -261,19 +262,29 @@ class PageTriageHooks {
 		}
 
 		// See if the page is in the PageTriage page queue
-		// If it isn't, $status will be null
-		$status = PageTriageUtil::doesPageNeedTriage( $article );
-		if ( !is_null( $status ) ) {
-			// See if the user created this page
-			if ( is_callable( array( 'WikiPage', 'getOldestRevision' ) )
-				&& $wgUser->getId() == $article->getOldestRevision()->getUser()
+		// If it isn't, $needsReview will be null
+		// Also, users without the autopatrol right can't review their own pages
+		$needsReview = PageTriageUtil::doesPageNeedTriage( $article );
+		if ( !is_null( $needsReview )
+			&& !( $wgUser->getId() == $article->getOldestRevision()->getUser()
 				&& !$wgUser->isAllowed( 'autopatrol' )
-			) {
-				// Do nothing since users without the autopatrol right can't review their own pages
-				return true;
-			} else {
+			)
+		) {
+			if ( $wgPageTriageEnableCurationToolbar ) {
 				// Load the JavaScript for the curation toolbar
 				$wgOut->addModules( 'ext.pageTriage.toolbarStartup' );
+			} else {
+				if ( $needsReview ) {
+					// show 'Mark as reviewed' link
+					$msg = wfMessage( 'pagetriage-markpatrolled' )->text();
+					$msg = Html::element( 'a', array( 'href' => '#', 'class' => 'mw-pagetriage-markpatrolled-link' ), $msg );
+				} else {
+					// show 'Reviewed' text
+					$msg = wfMessage( 'pagetriage-reviewed' )->escaped();
+				}
+				$wgOut->addModules( array( 'ext.pageTriage.article' ) );
+				$html = Html::rawElement( 'div', array( 'class' => 'mw-pagetriage-markpatrolled' ), $msg );
+				$wgOut->addHTML( $html );
 			}
 		}
 
