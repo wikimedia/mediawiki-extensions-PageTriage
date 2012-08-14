20b1df5ae164de516ca178911b101ed2b9b0fc1e
bsitu <bsitu@wikimedia.org>
2012-08-14 14:03:19 -0700

    PageTriage cleanup maintenance script
    Change-Id: I5ce46d88d8bf0dec1436d5e246adc7f28f33f0e8

---
+++ tools/cleanupPageTriage.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Remove page with namespace other than NS_MAIN/NS_USER from pagetriage queue
+ *
+ * @ingroup Maintenance
+ */
+
+require_once( dirname( __FILE__ ) . '/../../../maintenance/Maintenance.php' );
+
+/**
+ * Maintenance script that removes page with namespace other than NS_MAIN/NS_USER from pagetriage queue
+ *
+ * @ingroup Maintenance
+ */
+class CleanupPageTriage extends Maintenance {
+
+	protected $batchSize = 100;
+
+	public function execute() {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$count = $this->batchSize;
+		$start = 0;
+
+		while ( $count == $this->batchSize ) {
+			$res = $dbr->select(
+				array( 'pagetriage_page', 'page' ),
+				array( 'page_id' ),
+				array(
+					'page_id = ptrp_page_id',
+					'page_namespace != "' . NS_MAIN . '" AND page_namespace != "' . NS_USER . '"',
+					'ptrp_page_id > ' . $start
+				),
+				__METHOD__,
+				array( 'LIMIT' => $this->batchSize, 'ORDER BY' => 'ptrp_page_id' )
+			);
+
+			$page = array();
+			foreach( $res as $row ) {
+				$page[] = $row->page_id;
+				$start  = $row->page_id;
+			};
+			$count = count( $page );
+
+			if ( $count > 0 ) {
+				$dbw->begin();
+
+				$dbw->delete(
+					'pagetriage_page',
+					array( 'ptrp_page_id' => $page ),
+					__METHOD__
+				);
+
+				$dbw->delete(
+					'pagetriage_log',
+					array( 'ptrl_page_id' => $page ),
+					__METHOD__
+				);
+
+				$dbw->delete(
+					'pagetriage_page_tags',
+					array( 'ptrpt_page_id' => $page ),
+					__METHOD__
+				);
+
+				$dbw->commit();
+
+				$this->output( "processing " . $count . "\n" );
+				wfWaitForSlaves();
+			}
+
+		}
+	}
+}
+
+$maintClass = 'CleanupPageTriage'; // Tells it to run the class
+require_once( RUN_MAINTENANCE_IF_MAIN );
