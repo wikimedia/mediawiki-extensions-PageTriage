f4bb5c956a826caa32991b1b433952467a4e816c
rillke <rainerrillke@hotmail.com>
2013-09-11 22:04:47 +0200

    Use SQL that SQLite 3.7.8 understands
    When attempting to install the current extension code to a Wiki using a
    SQLite database version 3.7.8, installation will fail while running
    update.php because this SQLite version is unable to insert multiple
    rows - using the VALUES keyword - at once.
    
    Bug: 54013
    Change-Id: I90b0cc01fc747cb20f9bf9c79eebb2ada5b8fa5e

--- sql/PageTriageTags.sql
+++ sql/PageTriageTags.sql
@@ -8,21 +8,36 @@ CREATE TABLE /*_*/pagetriage_tags (
 CREATE UNIQUE INDEX /*i*/ptrt_tag_id ON /*_*/pagetriage_tags (ptrt_tag_name);
 
 INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
-VALUES 
-('linkcount', 'Number of inbound links'),
-('category_count', 'Category mapping count'), 
-('csd_status', 'CSD status'),
-('prod_status', 'PROD status'),
-('blp_prod_status', 'BLP PROD status'),
-('afd_status', 'AFD status'),
-('rev_count', 'Number of edits to the article'),
-('page_len', 'Number of bytes of article'),
-('snippet', 'Beginning of article snippet'),
-('user_name', 'User name'),
-('user_editcount', 'User total edit'),
-('user_creation_date', 'User registration date'),
-('user_autoconfirmed', 'Check if user is autoconfirmed' ),
-('user_bot', 'Check if user is in bot group'),
-('user_block_status', 'User block status'),
-('user_id', 'User id'),
-('reference', 'Check if page has references');
+VALUES ('linkcount', 'Number of inbound links');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('category_count', 'Category mapping count');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc)
+VALUES ('csd_status', 'CSD status');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('prod_status', 'PROD status');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('blp_prod_status', 'BLP PROD status');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('afd_status', 'AFD status');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('rev_count', 'Number of edits to the article');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('page_len', 'Number of bytes of article');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('snippet', 'Beginning of article snippet');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('user_name', 'User name');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('user_editcount', 'User total edit');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('user_creation_date', 'User registration date');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('user_autoconfirmed', 'Check if user is autoconfirmed' );
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('user_bot', 'Check if user is in bot group');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('user_block_status', 'User block status');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('user_id', 'User id');
+INSERT INTO /*_*/pagetriage_tags (ptrt_tag_name, ptrt_tag_desc) 
+VALUES ('reference', 'Check if page has references');
