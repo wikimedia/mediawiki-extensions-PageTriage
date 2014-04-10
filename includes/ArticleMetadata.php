c60aa68d163f08afa76eb386d1012f5ba5fcd7b7
bsitu <bsitu@wikimedia.org>
2014-04-10 11:23:25 -0700

    Use merge() to update cache data
    Change-Id: Iffe3b2c1477ac03101cb424bd88327cdfbfc2aa7

--- includes/ArticleMetadata.php
+++ includes/ArticleMetadata.php
@@ -57,12 +57,18 @@ class ArticleMetadata {
 			$pageId = $this->mPageId;
 		}
 
-		// @TODO: use merge() to make this atomic
 		foreach ( $pageId as $val ) {
-			$data = $wgMemc->get( $keyPrefix . '-' . $val );
-			if ( $data !== false ) {
-				$wgMemc->set( $keyPrefix . '-' . $val, array_merge( $data, $update ), 86400 );
-			}
+			$wgMemc->merge(
+				$keyPrefix . '-' . $val,
+				function( BagOStuff $cache, $key, $data ) use( $update ) {
+					if ( $data !== false ) {
+						return array_merge( $data, $update );
+					} else {
+						return $update;
+					}
+				},
+				86400
+			);
 		}
 	}
 
