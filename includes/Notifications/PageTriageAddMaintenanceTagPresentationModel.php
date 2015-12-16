6b2b2ecefae141dfb517b06c027f85d3c7f8e14f
Kunal Mehta <legoktm@member.fsf.org>
2015-12-16 11:11:17 -0800

    Convert spaces to tabs
    Change-Id: I60823d1f3d442951713f8182dd0e5da35ad24b3a

--- includes/Notifications/PageTriageAddMaintenanceTagPresentationModel.php
+++ includes/Notifications/PageTriageAddMaintenanceTagPresentationModel.php
@@ -1,56 +1,56 @@
 <?php
 
 class PageTriageAddMaintenanceTagPresentationModel extends EchoEventPresentationModel {
-    /**
-     * {@inheritdoc}
-     */
-    public function canRender() {
-        return $this->event->getTitle() instanceof Title;
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function canRender() {
+		return $this->event->getTitle() instanceof Title;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getIconType() {
-        return 'checkmark';
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getIconType() {
+		return 'checkmark';
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getPrimaryLink() {
-        return array(
-            'url' => $this->event->getTitle()->getFullURL(),
-            'label' => $this->msg( 'notification-link-text-view-page' )->text(),
-        );
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getPrimaryLink() {
+		return array(
+			'url' => $this->event->getTitle()->getFullURL(),
+			'label' => $this->msg( 'notification-link-text-view-page' )->text(),
+		);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getHeaderMessage() {
-        $msg = parent::getHeaderMessage();
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getHeaderMessage() {
+		$msg = parent::getHeaderMessage();
 
-        // parent adds agent & gender, this adds title as 3rd param, tag list as
-        // 4th & amount of tags as 5th (for PLURAL)
-        $msg->params( $this->event->getTitle() );
-        $msg->params( $this->getTagsForOutput() );
+		// parent adds agent & gender, this adds title as 3rd param, tag list as
+		// 4th & amount of tags as 5th (for PLURAL)
+		$msg->params( $this->event->getTitle() );
+		$msg->params( $this->getTagsForOutput() );
 
-        return $msg;
-    }
+		return $msg;
+	}
 
-    /**
-     * Returns an array of [tag list, amount of tags], to be used as msg params.
-     *
-     * @return array [(string) tag list, (int) amount of tags]
-     */
-    protected function getTagsForOutput() {
-        $eventData = $this->event->getExtra();
+	/**
+	 * Returns an array of [tag list, amount of tags], to be used as msg params.
+	 *
+	 * @return array [(string) tag list, (int) amount of tags]
+	 */
+	protected function getTagsForOutput() {
+		$eventData = $this->event->getExtra();
 
-        if ( !is_array( $eventData ) ) {
-            return array( '', 0 );
-        }
+		if ( !is_array( $eventData ) ) {
+			return array( '', 0 );
+		}
 
-        return array( $this->language->listToText( $eventData ), count( $eventData ) );
-    }
+		return array( $this->language->listToText( $eventData ), count( $eventData ) );
+	}
 }
--- includes/Notifications/PageTriageMarkAsReviewedPresentationModel.php
+++ includes/Notifications/PageTriageMarkAsReviewedPresentationModel.php
@@ -1,39 +1,39 @@
 <?php
 
 class PageTriageMarkAsReviewedPresentationModel extends EchoEventPresentationModel {
-    /**
-     * {@inheritdoc}
-     */
-    public function canRender() {
-        return $this->event->getTitle() instanceof Title;
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function canRender() {
+		return $this->event->getTitle() instanceof Title;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getIconType() {
-        return 'checkmark';
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getIconType() {
+		return 'checkmark';
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getPrimaryLink() {
-        return array(
-            'url' => $this->event->getTitle()->getFullURL(),
-            'label' => $this->msg( 'notification-link-text-view-page' )->text(),
-        );
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getPrimaryLink() {
+		return array(
+			'url' => $this->event->getTitle()->getFullURL(),
+			'label' => $this->msg( 'notification-link-text-view-page' )->text(),
+		);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getHeaderMessage() {
-        $msg = parent::getHeaderMessage();
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getHeaderMessage() {
+		$msg = parent::getHeaderMessage();
 
-        // parent adds agent & gender, this adds title as 3rd param
-        $msg->params( $this->event->getTitle() );
+		// parent adds agent & gender, this adds title as 3rd param
+		$msg->params( $this->event->getTitle() );
 
-        return $msg;
-    }
+		return $msg;
+	}
 }
