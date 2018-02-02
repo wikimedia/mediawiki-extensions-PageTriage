81d26e7cc8c1fa19896ee53fd405514f29733389
Umherirrender <umherirrender_de.wp@web.de>
2018-02-02 23:01:09 +0100

    Require MediaWiki 1.29 in extension.json for manifest_version 2
    manifest_version 2 is a new feature in 1.29
    (Iea5ba589c70958db7500cf3587b5ebd738532026)
    
    Change-Id: Ife34499f6ce3167a18edf2a49cce15f5d233853d

--- PageTriage.php
+++ PageTriage.php
@@ -11,5 +11,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	);*/
 	return;
 } else {
-	die( 'This version of the PageTriage extension requires MediaWiki 1.28+' );
+	die( 'This version of the PageTriage extension requires MediaWiki 1.29+' );
 }
