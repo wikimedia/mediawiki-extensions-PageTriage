59e1fedffcdafc46722d7231011aee194db9ee08
Sam Wilson <sam@samwilson.id.au>
2018-06-28 14:55:51 +0800

    Add maintenance script to backfill Draft NS pages into the triage queue
    Also excludes redirects. Even though they'd be caught later anyway,
    doing it here makes things a little bit quicker.
    
    Bug: T196181
    Change-Id: I09ff2307bab6719e67f4cfb1c1e5250ad3c25a02

---
+++ includes/Maintenance/PopulateDraftQueue.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace MediaWiki\Extension\PageTriage\Maintenance;
+
+use Maintenance;
+use MediaWiki\Extension\PageTriage\ArticleCompile\ArticleCompileProcessor;
+use MediaWiki\Extension\PageTriage\PageTriage;
+
+/**
+ * A maintenance script for populating the Draft namespace queue.
+ */
+class PopulateDraftQueue extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Add missing Draft namespace pages to the AfC triage queue.";
+		$this->requireExtension( 'PageTriage' );
+		$this->setBatchSize( 100 );
+	}
+
+	/**
+	 * @return bool|null True for success, false for failure.
+	 */
+	public function execute() {
+		// Find the Draft namespace ID.
+		$pageTriageDraftNamespaceId = $this->getConfig()->get( 'PageTriageDraftNamespaceId' );
+		if ( !$pageTriageDraftNamespaceId ) {
+			$this->output(
+				'Unable to determine Draft namespace. Please set $wgPageTriageDraftNamespaceId'
+			);
+			return false;
+		}
+
+		// Set up.
+		$this->output( "Processing drafts in NS $pageTriageDraftNamespaceId...\n" );
+		$db = $this->getDB( DB_MASTER );
+
+		// Loop through all batches.
+		$batchNum = 1;
+		$totalProcessed = 0;
+		$lastBatch = false;
+		while ( !$lastBatch ) {
+			// Progress indicator.
+			$this->output( "- batch $batchNum\n" );
+			$batchNum++;
+
+			// Find all Draft NS pages that don't have records in the PageTriage table.
+			$drafts = $db->select(
+				[ 'page', 'pagetriage_page' ],
+				[ 'page_id' ],
+				[
+					'ptrp_page_id' => null,
+					'page_namespace' => $pageTriageDraftNamespaceId,
+					'page_is_redirect' => '0',
+				],
+				__METHOD__,
+				[ 'LIMIT' => $this->getBatchSize() ],
+				[ 'pagetriage_page' => [ 'LEFT JOIN', [ 'page_id = ptrp_page_id' ] ] ]
+			);
+
+			// The loop will exit if this is the last batch.
+			if ( $drafts->numRows() < $this->getBatchSize() ) {
+				$lastBatch = true;
+			}
+
+			// Go through this batch, and add each page to the PageTriage queue.
+			foreach ( $drafts as $draft ) {
+				$this->beginTransaction( $db, __METHOD__ );
+				$pageTriage = new PageTriage( $draft->page_id );
+				$pageTriage->addToPageTriageQueue( '0' );
+				$acp = ArticleCompileProcessor::newFromPageId( [ $draft->page_id ] );
+				if ( $acp ) {
+					$acp->compileMetadata();
+				}
+				$this->commitTransaction( $db, __METHOD__ );
+				$totalProcessed++;
+			}
+		}
+
+		// Finish.
+		$this->output( "Complete; $totalProcessed drafts processed.\n" );
+		return true;
+	}
+
+}
---
+++ tests/phpunit/MaintenancePopulateDraftQueueTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace MediaWiki\Extension\PageTriage\Test;
+
+use MediaWiki\Extension\PageTriage\ArticleCompile\ArticleCompileAfcTag;
+use MediaWiki\Extension\PageTriage\Maintenance\PopulateDraftQueue;
+use PageTriageTestCase;
+
+/**
+ * Tests for the populateDraftQueueTest.php maintenance script.
+ * @group medium
+ */
+class MaintenancePopulateDraftQueueTest extends PageTriageTestCase {
+
+	public function setUp() {
+		parent::setUp();
+		$this->tablesUsed = [ 'pagetriage_page', 'pagetriage_page_tags', 'pagetriage_tags' ];
+		// Start with the Draft mode turned off
+		// (also use a different ID and NS name, just in case we're assuming these).
+		$this->draftNsId = 210;
+		$this->setMwGlobals( [
+			'wgExtraNamespaces' => [ $this->draftNsId => 'Submissions' ],
+			'wgPageTriageDraftNamespaceId' => false,
+		] );
+	}
+
+	/**
+	 * @covers \MediaWiki\Extension\PageTriage\Maintenance\PopulateDraftQueue
+	 */
+	public function testPreExistingPageAddedToDraftQueueAfterActivation() {
+		// Get the initial page count.
+		$initialCount = wfGetDB( DB_REPLICA )->selectRowCount( 'pagetriage_page' );
+		// Create a page in the Draft namespace and confirm that it hasn't been added to the
+		// PageTriage queue.
+		$testPage = $this->insertPage( self::class . 'Test1', '', $this->draftNsId );
+		$this->assertSelect( 'pagetriage_page', 'ptrp_page_id',
+			[ 'ptrp_page_id' => $testPage['id'] ], []
+		);
+
+		// Enable Drafts mode in PageTriage, and run the maintenance script.
+		$this->setMwGlobals( 'wgPageTriageDraftNamespaceId', $this->draftNsId );
+		$maint = new PopulateDraftQueue();
+		$maint->execute();
+		$this->expectOutputString(
+			"Processing drafts in NS {$this->draftNsId}...\n- batch 1\nComplete; 1 drafts processed.\n"
+		);
+
+		// Now the page should be in the queue.
+		$newCount = wfGetDB( DB_REPLICA )->selectRowCount( 'pagetriage_page' );
+		$this->assertEquals( $initialCount + 1, $newCount );
+	}
+
+	/**
+	 * @covers \MediaWiki\Extension\PageTriage\Maintenance\PopulateDraftQueue
+	 */
+	public function testPreExistingPagesWithCategoriesAreGivenCorrectTags() {
+		$testPageCount = 10;
+		// Get the initial page counts (because previous tests can leave things behind).
+		$db = wfGetDB( DB_REPLICA );
+		$initialCount = $db->selectRowCount( 'pagetriage_page' );
+		$initialAfCPendingCount = $db->selectRowCount(
+			[ 'pagetriage_page_tags', 'pagetriage_tags' ],
+			'*',
+			[
+				'ptrpt_tag_id = ptrt_tag_id',
+				'ptrt_tag_name' => 'afc_state',
+				'ptrpt_value' => ArticleCompileAfcTag::PENDING,
+			],
+			__METHOD__
+		);
+		// Add 10 pages, five with categories.
+		for ( $i = 1; $i <= $testPageCount; $i++ ) {
+			$text = ( $i % 2 ) ? '[[Category:Pending AfC submissions]]' : '';
+			$this->insertPage( self::class . 'TagTest' . $i, $text, $this->draftNsId );
+		}
+		// And one extra page, a redirect which shouldn't be included in the queue.
+		$this->insertPage( self::class . 'RedirectTest',
+			'#REDIRECT [[Non-draft page]]', $this->draftNsId
+		);
+		// No extra pages in the queue to start with.
+		$this->assertSelect( [ 'pagetriage_page' ], [ 'count' => 'COUNT(*)' ], [],
+			[ [ $initialCount ] ]
+		);
+
+		// Enable Drafts mode in PageTriage, and run the maintenance script.
+		$this->setMwGlobals( 'wgPageTriageDraftNamespaceId', $this->draftNsId );
+		$maint = new PopulateDraftQueue();
+		$maint->execute();
+		$this->expectOutputString(
+			"Processing drafts in NS {$this->draftNsId}...\n"
+			. "- batch 1\nComplete; $testPageCount drafts processed.\n"
+		);
+
+		// Now there are 10 more pages in the queue, 5 of them with the afc_state tag for 'pending'.
+		$this->assertSelect( [ 'pagetriage_page' ], [ 'count' => 'COUNT(*)' ], [],
+			[ [ 10 + $initialCount ] ]
+		);
+		$this->assertSelect(
+			[ 'pagetriage_page_tags', 'pagetriage_tags' ],
+			[ 'COUNT(*)' ],
+			[
+				'ptrpt_tag_id = ptrt_tag_id',
+				'ptrt_tag_name' => 'afc_state',
+				'ptrpt_value' => ArticleCompileAfcTag::PENDING,
+			],
+			[ [ 5 + $initialAfCPendingCount ] ]
+		);
+	}
+}
