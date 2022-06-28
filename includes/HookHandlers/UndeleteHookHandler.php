7b1f0eb1683d12484640983e3a50024c87258d35
Taavi Väänänen <hi@taavi.wtf>
2022-06-28 12:41:58 +0300

    Mark undeleted pages as unpatrolled
    Bug: T311347
    Change-Id: I18fb6037d1c1655bd21ab8ec5e5d79b73b9a4f1e

---
+++ includes/HookHandlers/UndeleteHookHandler.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Extension\PageTriage\HookHandlers;
+
+use MediaWiki\Extension\PageTriage\Hooks as PageTriageHooks;
+use MediaWiki\Page\Hook\ArticleUndeleteHook;
+use Title;
+
+/**
+ * @author Taavi Väänänen <hi@taavi.wtf>
+ */
+class UndeleteHookHandler implements ArticleUndeleteHook {
+	/**
+	 * @param Title $title
+	 * @param bool $create
+	 * @param string $comment
+	 * @param int $oldPageId
+	 * @param array $restoredPages
+	 */
+	public function onArticleUndelete( $title, $create, $comment, $oldPageId, $restoredPages ) {
+		if ( !$create ) {
+			// not interested in revdel actions
+			return;
+		}
+
+		PageTriageHooks::addToPageTriageQueue( $title->getId(), $title );
+	}
+}
