aea3668bc7f845d70c154b3be19aa1a1e40145de
Jon Robson <jdlrobson@gmail.com>
2023-05-10 08:26:05 -0700

    Remove unnecessary jQuery closure
    This is not needed here at all as no code is executed - only
    registered.
    
    It also means code running outside a closure can fail if executed
    before the document is ready.
    
    Bug: T324913
    Change-Id: I7e7a737230863d4d2488e5a4d8fd4f1dca64fa39
    (cherry picked from commit 234de9580151b3b0e699d3c6ee8d0a63bb995cd8)

--- modules/ext.pageTriage.util/models/ext.pageTriage.revision.js
+++ modules/ext.pageTriage.util/models/ext.pageTriage.revision.js
@@ -1,45 +1,42 @@
 // Revision represents a single revision (aka historyItem)
 // RevisionList is a collection of revisions for a single page
 //
-$( function () {
-	// sparse model because events don't work well when nesting these.
-	mw.pageTriage.Revision = Backbone.Model.extend( {} );
+// sparse model because events don't work well when nesting these.
+mw.pageTriage.Revision = Backbone.Model.extend( {} );
 
-	mw.pageTriage.RevisionList = Backbone.Collection.extend( {
-		model: mw.pageTriage.Revision,
+mw.pageTriage.RevisionList = Backbone.Collection.extend( {
+	model: mw.pageTriage.Revision,
 
-		apiParams: {
-			rvprop: 'timestamp|user|parsedcomment|ids',
-			rvlimit: 25 // get data for last 25 revisions
-		},
+	apiParams: {
+		rvprop: 'timestamp|user|parsedcomment|ids',
+		rvlimit: 25 // get data for last 25 revisions
+	},
 
-		initialize: function ( options ) {
-			this.eventBus = options.eventBus;
-			this.pageId = options.pageId;
-			this.setParam( 'pageids', options.pageId ); // pass this to the api
-		},
+	initialize: function ( options ) {
+		this.eventBus = options.eventBus;
+		this.pageId = options.pageId;
+		this.setParam( 'pageids', options.pageId ); // pass this to the api
+	},
 
-		url: function () {
-			return mw.util.wikiScript( 'api' ) + '?action=query&prop=revisions&format=json&' + $.param( this.apiParams );
-		},
+	url: function () {
+		return mw.util.wikiScript( 'api' ) + '?action=query&prop=revisions&format=json&' + $.param( this.apiParams );
+	},
 
-		parse: function ( response ) {
-			// extract the useful bits of json.
-			return response.query.pages[ this.pageId ].revisions;
-		},
+	parse: function ( response ) {
+		// extract the useful bits of json.
+		return response.query.pages[ this.pageId ].revisions;
+	},
 
-		setParams: function ( apiParams ) {
-			this.apiParams = apiParams;
-		},
+	setParams: function ( apiParams ) {
+		this.apiParams = apiParams;
+	},
 
-		setParam: function ( paramName, paramValue ) {
-			this.apiParams[ paramName ] = paramValue;
-		},
+	setParam: function ( paramName, paramValue ) {
+		this.apiParams[ paramName ] = paramValue;
+	},
 
-		getParam: function ( key ) {
-			return this.apiParams[ key ];
-		}
-
-	} );
+	getParam: function ( key ) {
+		return this.apiParams[ key ];
+	}
 
 } );
--- modules/ext.pageTriage.util/models/ext.pageTriage.stats.js
+++ modules/ext.pageTriage.util/models/ext.pageTriage.stats.js
@@ -1,80 +1,79 @@
 // Stats represents the dashboard data for pagetriage
-$( function () {
-	mw.pageTriage.Stats = Backbone.Model.extend( {
-		defaults: {
-			title: 'PageTriage Dashboard Data',
-			pageid: ''
-		},
 
-		apiParams: {
-			namespace: ''
-		},
+mw.pageTriage.Stats = Backbone.Model.extend( {
+	defaults: {
+		title: 'PageTriage Dashboard Data',
+		pageid: ''
+	},
 
-		initialize: function () {
-			this.bind( 'change', this.formatMetadata, this );
-		},
+	apiParams: {
+		namespace: ''
+	},
 
-		formatMetadata: function ( stats ) {
-			stats.set( 'afcMode', stats.get( 'namespace' ) === mw.config.get( 'wgPageTriageDraftNamespaceId' ) );
-			stats.set( 'ptrUnreviewedArticleCount', stats.get( 'unreviewedarticle' ).count );
-			stats.set( 'ptrUnreviewedRedirectCount', stats.get( 'unreviewedredirect' ).count );
-			stats.set( 'ptrOldest', this.formatDaysFromNow( stats.get( 'unreviewedarticle' ).oldest ) );
-			stats.set( 'ptrReviewedArticleCount', stats.get( 'reviewedarticle' ).reviewed_count );
-			stats.set( 'ptrReviewedRedirectCount', stats.get( 'reviewedredirect' ).reviewed_count );
-			if ( stats.get( 'afcMode' ) ) {
-				stats.set( 'ptrUnreviewedDraftCount', stats.get( 'unrevieweddraft' ).count );
-				stats.set( 'ptrUnreviewedDraftOldest', this.formatDaysFromNow( stats.get( 'unrevieweddraft' ).oldest ) );
-			}
-			stats.set( 'ptrFilterCount', stats.get( 'filteredarticle' ) );
-		},
+	initialize: function () {
+		this.bind( 'change', this.formatMetadata, this );
+	},
 
-		formatDaysFromNow: function ( dateStr ) {
-			if ( !dateStr ) {
-				return '';
-			}
-
-			let now = new Date();
-			now = new Date(
-				now.getUTCFullYear(),
-				now.getUTCMonth(),
-				now.getUTCDate(),
-				now.getUTCHours(),
-				now.getUTCMinutes(),
-				now.getUTCSeconds()
-			);
-
-			const begin = moment.utc( dateStr );
-
-			const diffInDays = Math.round( ( now.getTime() - begin.valueOf() ) / ( 1000 * 60 * 60 * 24 ) );
-			if ( diffInDays ) {
-				return mw.msg( 'days', mw.language.convertNumber( diffInDays ) );
-			} else {
-				return mw.msg( 'pagetriage-stats-less-than-a-day', mw.language.convertNumber( diffInDays ) );
-			}
-		},
-
-		setParam: function ( paramName, paramValue ) {
-			this.apiParams[ paramName ] = paramValue;
-		},
-
-		url: function () {
-			let url = mw.util.wikiScript( 'api' ) + '?action=pagetriagestats&format=json';
-			if ( this.apiParams.namespace !== '' ) {
-				// afc_state is stored in the model as '1', '2', '3', '4', or 'all',
-				// but the api parameter should be an integer. Omitting the parameter
-				// entirely means there is no filtering, which is what 'all' should
-				// do. See T304574
-				if ( this.apiParams.afc_state && this.apiParams.afc_state === 'all' ) {
-					delete this.apiParams.afc_state;
-				}
-				url += '&' + $.param( this.apiParams );
-			}
-			return url;
-		},
-
-		parse: function ( response ) {
-			// extract the useful bits of json.
-			return response.pagetriagestats.stats;
+	formatMetadata: function ( stats ) {
+		stats.set( 'afcMode', stats.get( 'namespace' ) === mw.config.get( 'wgPageTriageDraftNamespaceId' ) );
+		stats.set( 'ptrUnreviewedArticleCount', stats.get( 'unreviewedarticle' ).count );
+		stats.set( 'ptrUnreviewedRedirectCount', stats.get( 'unreviewedredirect' ).count );
+		stats.set( 'ptrOldest', this.formatDaysFromNow( stats.get( 'unreviewedarticle' ).oldest ) );
+		stats.set( 'ptrReviewedArticleCount', stats.get( 'reviewedarticle' ).reviewed_count );
+		stats.set( 'ptrReviewedRedirectCount', stats.get( 'reviewedredirect' ).reviewed_count );
+		if ( stats.get( 'afcMode' ) ) {
+			stats.set( 'ptrUnreviewedDraftCount', stats.get( 'unrevieweddraft' ).count );
+			stats.set( 'ptrUnreviewedDraftOldest', this.formatDaysFromNow( stats.get( 'unrevieweddraft' ).oldest ) );
 		}
-	} );
+		stats.set( 'ptrFilterCount', stats.get( 'filteredarticle' ) );
+	},
+
+	formatDaysFromNow: function ( dateStr ) {
+		if ( !dateStr ) {
+			return '';
+		}
+
+		let now = new Date();
+		now = new Date(
+			now.getUTCFullYear(),
+			now.getUTCMonth(),
+			now.getUTCDate(),
+			now.getUTCHours(),
+			now.getUTCMinutes(),
+			now.getUTCSeconds()
+		);
+
+		const begin = moment.utc( dateStr );
+
+		const diffInDays = Math.round( ( now.getTime() - begin.valueOf() ) / ( 1000 * 60 * 60 * 24 ) );
+		if ( diffInDays ) {
+			return mw.msg( 'days', mw.language.convertNumber( diffInDays ) );
+		} else {
+			return mw.msg( 'pagetriage-stats-less-than-a-day', mw.language.convertNumber( diffInDays ) );
+		}
+	},
+
+	setParam: function ( paramName, paramValue ) {
+		this.apiParams[ paramName ] = paramValue;
+	},
+
+	url: function () {
+		let url = mw.util.wikiScript( 'api' ) + '?action=pagetriagestats&format=json';
+		if ( this.apiParams.namespace !== '' ) {
+			// afc_state is stored in the model as '1', '2', '3', '4', or 'all',
+			// but the api parameter should be an integer. Omitting the parameter
+			// entirely means there is no filtering, which is what 'all' should
+			// do. See T304574
+			if ( this.apiParams.afc_state && this.apiParams.afc_state === 'all' ) {
+				delete this.apiParams.afc_state;
+			}
+			url += '&' + $.param( this.apiParams );
+		}
+		return url;
+	},
+
+	parse: function ( response ) {
+		// extract the useful bits of json.
+		return response.pagetriagestats.stats;
+	}
 } );
