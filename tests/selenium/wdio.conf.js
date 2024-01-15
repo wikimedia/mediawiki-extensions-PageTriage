395d8baddaab4a9feb185c3e410cc53c4aaec22c
WMDE-Fisch <christoph.jauera@wikimedia.de>
2024-01-15 13:17:51 +0100

    selenium: run tests concurrently
    Bug: T226869
    Change-Id: I959b957a5ebbc693075fc8089863ae3c9b611e81

--- tests/selenium/wdio.conf.js
+++ tests/selenium/wdio.conf.js
@@ -6,6 +6,7 @@ const { config } = require( 'wdio-mediawiki/wdio-defaults.conf.js' ),
 exports.config = { ...config,
 	// Override, or add to, the setting from wdio-mediawiki.
 	// Learn more at https://webdriver.io/docs/configurationfile/
+	maxInstances: 4,
 	onPrepare: async function () {
 		await LocalSettingsSetup.overrideLocalSettings();
 		await LocalSettingsSetup.restartPhpFpmService();
